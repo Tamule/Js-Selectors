417479e790a33872550ae4134e0bd9d4c8d1027f
Tamule <122865779+Tamule@users.noreply.github.com>
2023-02-10 16:11:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//1// 
+ let specialCountry = document.querySelector("#special");
+console.log (specialCountry);
+
+//2//
+let countries = document.querySelectorAll(".country");
+console.log(countries);
+
+//3//
+specialCountry.classList.add("special");
+
+//4//
+specialCountry.innerHTML = "Las Vegas"
